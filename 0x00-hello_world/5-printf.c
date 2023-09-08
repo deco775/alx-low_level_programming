600487b05f137ea3425f76c25b9db12a40a40209
deco775 <mkansipitso@gmail.com>
2023-09-07 17:54:14 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdi.h>
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the utcoe is a piece of art,\n");
+	return (0);
+}
