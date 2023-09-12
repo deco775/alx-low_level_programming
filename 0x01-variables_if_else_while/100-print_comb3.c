ddcce47994bb19d423fefa0ee20d6a7080752813
deco775 <mkansipitso@gmail.com>
2023-09-11 20:20:21 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - prints all the possible diffferent combinatonsof two digits.
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int n, m;
+	for(n=48; n<=57; n++)
+	{
+		for (m=49; m<=57; m++)
+		{
+			if(m>n)
+			{
+				putchar(n);
+				putchar(m);
+				if(n!=56 || m !- 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
